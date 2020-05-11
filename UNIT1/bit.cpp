119d309cacf374716f5daf13382d4de749ee7b60
Girish-21706 <32380394+Girish-21706@users.noreply.github.com>
2020-05-11 09:35:56 +0530

    bit.cpp
    bitwise operator

---
+++ UNIT1/bit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main()
+{
+  int a=8,b=4,c;
+   
+  c = a&b;
+  cout<<"Result of & : "<<c<<endl;
+  c = a|b;
+  cout<<"Result of | : "<<c<<endl;
+  c = a^b;
+  cout<<"Result of ^ : "<<c<<endl;
+   
+  c = a<<2;
+  cout<<"Result of << by 2 bits : "<<c<<endl; c = b>>2;
+  cout<<"Result of >> by 2 bits : "<<c<<endl;
+   
+  c = ~3;
+  cout<<"Result of ~ : "<<c<<endl;
+}
